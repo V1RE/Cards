9cc5170388bd0d2eb4ef85f7c12eb8327167984f
Niels Mentink <niels@ment.ink>
2020-10-05 00:52:50 +0200

    Added basic firebase

---
+++ src/pages/[username]/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let username;
+
+  import { Collection } from "sveltefire";
+  import Firebase from "../../components/Firebase.svelte";
+</script>
+
+<Firebase>
+  <Collection
+    path={'users'}
+    query={(ref) => ref.where('username', '==', username)}
+    let:data={curUser}>
+    <div slot="fallback">error</div>
+    <div slot="loading">loading</div>
+    <h2>Hello {curUser[0].firstname}</h2>
+  </Collection>
+</Firebase>
